49ffc6afa2a70d13dd3360ad4bc0fb62cdbecd01
je06jm <jeremymoyes3@gmail.com>
2021-12-20 20:58:15 -0700

    Fixed port header

--- include/Emu8086/port.h
+++ include/Emu8086/port.h
@@ -8,10 +8,13 @@ typedef void (*Emu8086_Port_FuncWriteByte)(byte_t port, byte_t value);
 typedef word_t (*Emu8086_Port_FuncReadWord)(word_t port);
 typedef void (*Emu8086_Port_FuncWriteWord)(word_t port, word_t value);
 
-void Emu8086_Port_RegisterReadByte(Emu8086_Port_FuncReadByte* device);
-void Emu8086_Port_RegisterWriteByte(Emu8086_Port_FuncWriteByte* device);
-void Emu8086_Port_RegisterReadWord(Emu8086_Port_FuncReadWord* device);
-void Emu8086_Port_RegisterWriteWord(Emu8086_Port_FuncWriteWord* device);
+void Emu8086_Port_Init();
+void Emu8086_Port_Finish();
+
+void Emu8086_Port_RegisterReadByte(Emu8086_Port_FuncReadByte device);
+void Emu8086_Port_RegisterWriteByte(Emu8086_Port_FuncWriteByte device);
+void Emu8086_Port_RegisterReadWord(Emu8086_Port_FuncReadWord device);
+void Emu8086_Port_RegisterWriteWord(Emu8086_Port_FuncWriteWord device);
 
 byte_t Emu8086_Port_ReadByte(byte_t port);
 void Emu8086_Port_WriteByte(byte_t port, byte_t value);
